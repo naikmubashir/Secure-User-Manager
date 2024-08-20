8aaf82c2a82fcd1dcb9cb605dad5747cff98a07f
Naik Mubashir <naikmubashir095@gmail.com>
2024-08-20 15:11:48 +0530

    Modify root Router

--- routes/root.js
+++ routes/root.js
@@ -4,6 +4,14 @@ const path = require('path');
 
 router.get('^/$|/index(.html)?', (req,res)=>{
     res.status(200).sendFile(path.join(__dirname,'../views','index.html'));
+});
+
+router.get('/newpage(.html)?',(req,res)=>{
+    res.sendFile(path.join(__dirname,"../views",'new-page.html'));
+});
+
+router.get('/oldPage(.html)?', (req,res)=>{
+    res.redirect(301,'/newPage.html'); //302/304 is default for redirect.. 301 is for permanent redirect
 })
 
 module.exports =router;
