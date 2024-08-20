0bd5a42f6c2d5898a0717dcd445643fe809f63d4
Naik Mubashir <naikmubashir095@gmail.com>
2024-08-20 13:11:44 +0530

    Check DB connection before listening

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 
 require('dotenv').config(); // to load environment variables from a .env file into process.env
-
 const express = require("express");
 const connectDB = require('./config/DBconn');
 const app = express();
 const PORT = process.env.PORT || 3500;
+const mongoose = require('mongoose');
+
 connectDB();
 
-app.listen(PORT, ()=>console.log(  `app connected to port ${PORT}`))
+
+//making sure to only listen when the database is connected
+mongoose.connection.once("open",()=>{
+    console.log("Connected to MongoDB....");
+    app.listen(PORT, ()=>console.log(  `app connected to port ${PORT}`))
+})
